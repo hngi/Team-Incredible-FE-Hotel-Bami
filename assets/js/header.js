9166bece199fe82336a1018b2f828c02898c6451
tosinibrahim96 <tosinibrahim96@gmail.com>
2020-06-09 23:15:04 +0100

    load header.js files properly in html files and fix mobile navigation overlap

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,10 +1,14 @@
 const handleResponsiveHeader = () => {
-  console.log("here");
+
   const hamburger = document.querySelector(".hamburger");
   const navLinks = document.querySelector(".nav-links");
+  const navbar = document.querySelector(".nav");
 
   hamburger.addEventListener("click", () => {
+    nextElement = navbar.nextElementSibling;
+
     navLinks.classList.toggle("nav-open");
+    nextElement.classList.toggle("push-down");
   });
 };
 
