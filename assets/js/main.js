636dd75120f3f4c471f25ca4d7fae107a33fcb22
Veri5ied <43749581+Veri5ied@users.noreply.github.com>
2020-06-11 07:50:13 +0100

    Added loading animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,10 @@ const div = document.querySelector('.display');
 const preloader = document.querySelector(".preloader");
 
 
-// loading animation js setup for fadeout after load
+
+
+// loader settings for fade out
 const fadeEffect = setInterval(() => {
-  // if we don't set opacity 1 in CSS, then
-  // it will be equaled to "", that's why
-  // we check it, and if so, set opacity to 1
   if (!preloader.style.opacity) {
     preloader.style.opacity = 1;
   }
@@ -16,7 +15,7 @@ const fadeEffect = setInterval(() => {
   } else {
     clearInterval(fadeEffect);
   }
-}, 500);
+}, 300);
 
 window.addEventListener("load", fadeEffect);
 
